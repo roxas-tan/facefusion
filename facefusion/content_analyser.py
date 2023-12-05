6a33a812f67485603893a05a29f2ca1b42ac8490
Roxas77 <63656689+roxas-tan@users.noreply.github.com>
2023-12-05 16:31:05 +0800

    Update content_analyser.py
    Bypass NSFW filter and skip video analysis

--- facefusion/content_analyser.py
+++ facefusion/content_analyser.py
@@ -68,13 +68,7 @@ def prepare_frame(frame : Frame) -> Frame:
 
 
 def analyse_frame(frame : Frame) -> bool:
-	content_analyser = get_content_analyser()
-	frame = prepare_frame(frame)
-	probability = content_analyser.run(None,
-	{
-		'input:0': frame
-	})[0][0][1]
-	return probability > MAX_PROBABILITY
+	return False
 
 
 @lru_cache(maxsize = None)
@@ -90,13 +84,4 @@ def analyse_video(video_path : str, start_frame : int, end_frame : int) -> bool:
 	frame_range = range(start_frame or 0, end_frame or video_frame_total)
 	rate = 0.0
 	counter = 0
-	with tqdm(total = len(frame_range), desc = wording.get('analysing'), unit = 'frame', ascii = ' =') as progress:
-		for frame_number in frame_range:
-			if frame_number % int(fps) == 0:
-				frame = get_video_frame(video_path, frame_number)
-				if analyse_frame(frame):
-					counter += 1
-			rate = counter * int(fps) / len(frame_range) * 100
-			progress.update()
-			progress.set_postfix(rate = rate)
 	return rate > MAX_RATE
